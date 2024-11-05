26aae8a4bb47e7bcddb8508080894ffdd755ca8b
msmecum1 <msmecum1@gmail.com>
2024-11-04 22:25:14 -0600

    first push

---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using DiceRollGame.Game;
+
+var random = new Random();
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+
+GameResult gameResult = guessingGame.Play();
+GuessingGame.PrintResult(gameResult);
+
+Console.ReadKey();
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
